7da3f3b760707e8967da8d70e719b79398f5e734
Christy <hchristy1234@gmail.com>
2021-12-07 13:23:23 -0800

    dec 7

---
+++ dec6/dec6.java
@@ -0,0 +1,13 @@
+public class dec6 {
+    public static void main(String[] args) {
+        
+    }
+    public List<Integer> getBlankPositions() {
+        String x = sentence;
+        ArrayList<Integer> = new ArrayList<Integer>();
+        while (x.length() > 0) {
+            int index = x.indexOf(" ");
+            String x = x.substring()
+        }
+    }
+}
