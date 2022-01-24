9d58047d9b9d6cf91f607adcb39e2cbc08e276f2
Christy <hchristy1234@gmail.com>
2022-01-24 13:15:06 -0800

    jan 24

---
+++ jan24.java
@@ -0,0 +1,15 @@
+public class jan24 {
+    // private shoes; // abstract -- // --> heels 
+    // cannot do shoes x
+    // can do heels x
+
+    // can show circle. can show quadrilateral. can show triangle. cannot show shape. so make shape abstract.
+
+    // use public abstract class Shoe
+    // cannot do Shoe x = new Shoe(); anymomre
+    // can still do Heel x = new Heel();
+    // can still do Shoe y = new Heel();
+    // can do Shoe[] shelf = new Shoe[4];
+    // can do ArrayList<Shoe> xy = new ArrayList<Shoe>();
+    // basically the only thing you can't create is a pure shoe - everything else you can do
+}
