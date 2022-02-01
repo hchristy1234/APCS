4edabb0c49e63d0fe829db85d2ad40592acec07f
Christy <hchristy1234@gmail.com>
2022-02-01 13:18:34 -0800

    feb1

---
+++ feb1.java
@@ -0,0 +1,13 @@
+public class feb1 {
+    // bubble sort: typically run n-1 iterations
+    // n(n-1) = n^2 - n = O(n^2)
+    // smart bubble sort: stops when all the numbers are sorted
+    // best case: O(n); average case O(n^2); worst case O(n^2)
+
+    // selection sort: grabbing smallest number and swapping with largest number. add marker.
+    // O(n^2) all cases
+
+    // insertion sort
+    // grab next one and sort along your own sorted list [left side]
+    // BC O(n); AC O(n^2); WC O(n^2)
+}
